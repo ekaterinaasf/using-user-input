8119e344c15a0489b969c70cee29d834bf159d37
ekaterinaasf <59531760+ekaterinaasf@users.noreply.github.com>
2020-02-04 21:06:52 +0100

    Update listeners.js

--- 1-fill-in-the-blanks/fonting/scripts/listeners.js
+++ 1-fill-in-the-blanks/fonting/scripts/listeners.js
@@ -1,1 +1,12 @@
-document.getElementById('user-input').addEventListener('change', __);
+document.getElementById('user-input').addEventListener('keyup', editorClassesHandler);
+//document.getElementById('user-action').addEventListener('click', typeCastHandler);
+//document.getElementById('user-input').addEventListener('keyup', capsLockHandler);
+/* 
+mouse events (MouseEvent): mousedown, mouseup, click, dblclick, mousemove, mouseover, mousewheel, mouseout, contextmenu
+touch events (TouchEvent): touchstart, touchmove, touchend, touchcancel
+
+keyboard events (KeyboardEvent): keydown, keypress, keyup 
+https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+
+form events: focus, blur, change, submit
+window events: scroll, resize, hashchange, load, unload */
