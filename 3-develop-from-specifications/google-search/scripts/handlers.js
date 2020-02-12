792825ff4a40a420f923b628bfa2d3d1f8fe9197
Ekaterina <nataveres@gmail.com>
2020-02-12 12:51:13 +0100

    Google-search exercise finished. Not sure, but maybe we should use google API??

--- 3-develop-from-specifications/google-search/scripts/handlers.js
+++ 3-develop-from-specifications/google-search/scripts/handlers.js
@@ -1,12 +1,13 @@
-function handler(event) {
+function gooooogle(event) {
   // read & process user input
-
+const userQuery = event.target.form.query.value;
 
   // call the core logic
-
+  const variable = googleURL(userQuery);
 
   // render results for the user
-
+  //Open a new tab
+  window.open(variable);
 
   // log action for developers
   console.log('\n-- user action --');
--- 3-develop-from-specifications/google-search/scripts/logic.js
+++ 3-develop-from-specifications/google-search/scripts/logic.js
@@ -1,16 +1,20 @@
-const _Tests = [
-  { name: 'first', args: [], expected: null },
-  { name: 'second', args: [], expected: null },
-  { name: 'third', args: [], expected: null },
-  { name: 'fourth', args: [], expected: null },
-  { name: 'fifth', args: [], expected: null },
-  { name: 'sixth', args: [], expected: null },
-  { name: 'seventh', args: [], expected: null },
-  { name: 'eighth', args: [], expected: null },
-  { name: 'ninth', args: [], expected: null },
-  { name: 'tenth', args: [], expected: null },
+const googleURLTests = [
+  { name: 'first', args: ['money'], expected: 'https://www.google.com/search?q=money' },
+  { name: 'second', args: [''], expected: 'https://www.google.com/search?q=' },
+  { name: 'third', args: ['javascript'], expected: 'https://www.google.com/search?q=javascript' },
+  { name: 'fourth', args: ['1'], expected: 'https://www.google.com/search?q=1' },
+  { name: 'fifth', args: ['hackyourfuture'], expected: 'https://www.google.com/search?q=hackyourfuture'},
+  { name: 'sixth', args: ['null'], expected: 'https://www.google.com/search?q=null' },
+  { name: 'seventh', args: ['true'], expected: 'https://www.google.com/search?q=true' },
+  { name: 'eighth', args: ['error'], expected: 'https://www.google.com/search?q=error' },
+  { name: 'ninth', args: ['Google API'], expected: 'https://www.google.com/search?q=Google API' },
+  { name: 'tenth', args: ['last one'], expected: 'https://www.google.com/search?q=last one' },
 ];
 
-function _() { }
+function googleURL(searchQuery) { 
+  return ('https://www.google.com/search?q='+searchQuery);
 
-testing(_, _Tests);
+}
+//https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters
+
+testing(googleURL, googleURLTests);
